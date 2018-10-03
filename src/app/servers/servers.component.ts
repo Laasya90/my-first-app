759a9e7f3a637f991242cb362ea51d61fdf02ea3
LaasyaPriyaPendyala <laasyapriya.pendyala@bt.com>
2018-10-03 16:42:06 +0530

    creating an angular app for practice

---
+++ src/app/servers/servers.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  //selector: 'app-servers',
+  //selector: '[app-servers]',
+  selector:'.app-servers',
+  // template:`<app-server></app-server>
+  //           <app-server></app-server>`,
+  templateUrl: './servers.component.html',
+  styleUrls: ['./servers.component.css']
+})
+export class ServersComponent implements OnInit {
+  
+  allowNewServer = false;
+  serverCreationStatus = "no server was created";
+  serverName = 'server1';
+  constructor() { 
+    setTimeout(() => {
+      this.allowNewServer = true;
+    },2000);
+  }
+
+  ngOnInit() {
+  }
+
+  onCreateServer(){
+    this.serverCreationStatus = "server created" + this.serverName;
+  }
+
+  //event is of type 'Event'
+  onUpdateServer(event: Event){
+    console.log(event);
+    this.serverName = (<HTMLInputElement>event.target).value;
+  }
+
+}
